b1f67a8266cd06a30047ff8e7ee23bdc204d4a31
mdeasinkhandokar <easinkhandokar2000@gmail.com>
2025-12-29 00:40:25 +0600

    login form

---
+++ CRUD/login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login </title>
+     <link rel="stylesheet" href="login_style.css">
+</head>
+<body>
+    <div class ="center">
+        <h1>Login</h1>
+
+        <div class="form">
+            <input type="text" name=""class="textfiled"placeholder="username">
+            <input type="password" name=""class="textfiled"placeholder="password">
+
+           <div class="forgetpass"><a href="#" class="link">Forget Password ?</a></div>   
+           <input type="submit" name="" value="Login" class="btn">     
+           
+           <div class="signup">New Member ?<a href="#" class="link">SignUp Here</a></div>
+
+
+        </div>
+
+    </div>
+    
+</body>
+</html>
